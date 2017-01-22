450d802e682796708b3b935045d54ed6c71c917c
Ye Wang <wangye@stanford.edu>
2017-01-21 16:13:14 -0800

    Made a simple stage that tilts, and a ball that rolls.

---
+++ CS248FinalProject/Assets/Scripts/StageTilt.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageTilt : MonoBehaviour {
+	private float xAngle;
+	private float zAngle;
+	public float restoringforce;
+	// Use this for initialization
+	void Start () {
+		xAngle = 0f;
+		zAngle = 0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		xAngle = Input.GetAxis ("Horizontal");
+		xAngle += (0 - xAngle)*restoringforce;
+		zAngle = Input.GetAxis ("Vertical");
+		zAngle += (0 - zAngle)*restoringforce;
+		transform.Rotate(new Vector3(xAngle, 0, zAngle));
+		
+	}
+}
