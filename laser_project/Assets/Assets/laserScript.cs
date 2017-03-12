b6916dc2378daed165b7e53f55af601b07a9ec8b
Flyhigh2017 <yicheng@stanford.edu>
2017-03-12 16:01:26 -0700

    laser
    laser

---
+++ laser_project/Assets/Assets/laserScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class laserScript : MonoBehaviour {
+
+	// Use this for initialization
+	public Transform startPoint;
+	public Transform endPoint;
+	LineRenderer laserLine;
+	void Start () {
+		laserLine = GetComponent<LineRenderer> ();
+		laserLine.startWidth = 0.2f;
+		laserLine.endWidth = 0.2f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		laserLine.SetPosition (0, startPoint.position);
+		laserLine.SetPosition (1, endPoint.position);
+	}
+}
