f00fe35a834d4c12857d0cd5a2b67cc0e8d6d620
Ye Wang <wangye@stanford.edu>
2017-03-22 06:18:13 -0700

    temp commit again...

--- CS248FinalProject/Assets/Scripts/MazeMaker.cs
+++ CS248FinalProject/Assets/Scripts/MazeMaker.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.IO;
 
 public class MazeMaker : MonoBehaviour {
-	public TextAsset stageFile;
+	public String stageFileName;
 	public int width, height;
 	public Material brick = null;
 	private int[,] Maze;
@@ -90,15 +90,14 @@ public class MazeMaker : MonoBehaviour {
 
 	// =======================================
 	public int[,] CreateMaze() {
-		string stageInfo = stageFile.text;
-		string[] stageInfoArray = stageInfo.Split ('\n');
-		height = Convert.ToInt32(stageInfoArray[0].Trim());
-		width = Convert.ToInt32(stageInfoArray[1].Trim());
+		StreamReader fileReader = new StreamReader (stageFileName, Encoding.Default);
+
+		height = Convert.ToInt32(fileReader.ReadLine().Trim());
+		width = Convert.ToInt32(fileReader.ReadLine().Trim());
 		worldOffset = new Vector3 (-width / 2f, 1f, height / 2f); 
 		Maze = new int[width, height];
-		for (int z = 0; z < height; z++) {		
-			// Debug.Log (stageInfoArray[2+z]);
-			string currLine = stageInfoArray[2 + z].Trim ();
+		for (int z = 0; z < height; z++) {			
+			string currLine = fileReader.ReadLine ().Trim ();
 			for (int x = 0; x < width; x++) {
 				Char currBlock = currLine [x];
 				switch (currBlock) {
@@ -112,13 +111,6 @@ public class MazeMaker : MonoBehaviour {
 					Maze [x, z] = 0;
 					GameObject playerInstance = setObject (x, 0, z, playerObj, Quaternion.identity);
 					Camera.main.GetComponent<FollowCharacter> ().playerObject = playerInstance;
-					CharacterControl charControl = playerInstance.GetComponent<CharacterControl> ();
-					charControl.GameOverUI = GameObject.FindGameObjectWithTag ("GameOverUI");
-					charControl.SuccessUI = GameObject.FindGameObjectWithTag ("SuccessUI");
-					charControl.Menu = GameObject.FindGameObjectWithTag ("Menu");
-					charControl.GameOverUI.SetActive (false);
-					charControl.Menu.SetActive (false);
-					charControl.SuccessUI.SetActive (false);
 					break;
 				case 'G':
 					Maze [x, z] = 0;
--- CS248FinalProject/Assets/Scripts/TimeControl.cs
+++ CS248FinalProject/Assets/Scripts/TimeControl.cs
@@ -1,37 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class TimeControl : MonoBehaviour {
 	public float timescale = 1f;
-	private bool paused;
-	public Button pauseButton;
-	public Sprite pause;
-	public Sprite play;
-	public AudioSource gameAudio;
 
 	// Use this for initialization
 	void Start () {
 		Time.timeScale = timescale;
-		paused = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
-	}
-
-	public void Pause() {
-		if (paused) {
-			Time.timeScale = timescale;
-			pauseButton.image.sprite = pause;
-			gameAudio.Play ();
-		} else {
-			Time.timeScale = 0f;
-			pauseButton.image.sprite = play;
-			gameAudio.Pause ();
-		}
-		paused = !paused;
 	}
 }
